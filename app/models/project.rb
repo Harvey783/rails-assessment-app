824555e3f99185fd22527bc46d952c55e857f43c
Christopher Harvey <chharvey@icloud.com>
2018-05-16 20:10:30 -0400

    #activityattributes

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,10 @@ class Project < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
+
+  def activity_attributes=(activity_attributes)
+    if activity_attributes[:name].present?
+      self.activities.build(activity_attributes)
+    end
+  end
 end
